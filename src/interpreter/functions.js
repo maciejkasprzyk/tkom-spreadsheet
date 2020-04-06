35497192e3275704e548c804c2462b08482a803b
Maciej Kasprzyk <maciejkasprzyk98@gmail.com>
2020-04-06 20:19:16 +0200

    added avg function

--- src/interpreter/functions.js
+++ src/interpreter/functions.js
@@ -1,4 +1,13 @@
-export const functions = {
-  "sum": (list) => list.reduce((a, b) => a + b, 0)
+function sum(list) {
+  return list.reduce((a, b) => a + b, 0);
+}
 
+function avg(list) {
+  return sum(list) / sum.length;
+}
+
+
+export const functions = {
+  "sum": sum,
+  "avg": avg,
 };
